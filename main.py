7f499b668ca64705ad78e260b19888989138a8bc
Ruslan Kayumov <rkau@ya.ru>
2021-10-09 12:51:50 +0300

    blank version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tg.bot as bot
 import yobit.yobit as yobit
 
+# активация бота (поддержка команды start)
 updater, dispatcher = bot.InitBot()
 bot.ActivateBot(dispatcher, updater)
 
+#см. tg/bot.py, yobit/yobit.py
